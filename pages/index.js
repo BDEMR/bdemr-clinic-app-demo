fecf6214a06aa9f2f6c75df60dff5300f530ec9a
rabbi011 <rakibil15-13011@diu.edu.bd>
2022-10-07 01:14:51 +0600

    console done

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import { Box, Button, Checkbox, FormControl, FormControlLabel, FormGroup, InputLabel, OutlinedInput, Select, Table, TableCell, TableContainer, TableHead, TableRow, TextField } from '@mui/material';
 import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
-
+import DeleteIcon from '@mui/icons-material/Delete';
 import Paper from '@mui/material/Paper';
+import { useState } from 'react';
 
 
 
@@ -11,9 +12,101 @@ import Paper from '@mui/material/Paper';
 export default function Home() {
 
 
+  const [search, setSearch] = useState('')
+  const [amount, setAmount] = useState('')
+  const [qun, setQun] = useState('')
+  const [items, setItems] = useState([])
+  const [discounts,setDiscounts] = useState('')
+  const [sum,setSum] = useState()
 
 
-  const router = useRouter();
+  const handelSearch = (e) => {
+    setSearch(e.target.value);
+
+  }
+
+
+
+  const handelAmount = (e) => {
+    setAmount(e.target.value)
+    setQun(1)
+  }
+
+
+  const handelAddItem = (e) => {
+
+    setItems(allItem)
+
+  }
+
+
+
+  const allItem = [
+
+    {
+
+      "search": { search },
+      "amount": { amount },
+      "key": 1,
+      "quantity": { qun },
+      "total": '1',
+
+
+    }
+
+
+  ]
+
+
+
+  const add = () => {
+    
+      const q = qun + 2;
+      setQun(q)
+      handelAddItem()
+    
+  }
+  const sub = () =>{
+    const q = qun - 1;
+    setQun(q)
+    handelAddItem()
+  }
+
+  const totalBill = qun* amount;
+
+
+  const handelDiscount = (e) =>{
+    const dis = (e.target.value);
+    const totalBill = qun* amount;
+   const di = totalBill - parseInt(dis)
+   if(totalBill == dis){
+    setDiscounts(0);
+   }
+   else{
+    setDiscounts(di);
+   }
+   
+    
+  }
+
+
+
+  const handelVat = (e) =>{
+    const vat = e.target.value;
+    if(discounts){
+      const finalvalue =discounts + (vat * 0.01 * discounts) ;
+      setDiscounts(finalvalue)
+    }
+    else{
+      const finalvalue = totalBill + (vat * 0.01 * totalBill)
+      setDiscounts(finalvalue)
+    }
+  }
+
+
+
+
+
   return (
     <div className={styles.container}>
       <section >
@@ -44,58 +137,50 @@ export default function Home() {
 
 
       <section>
-        <h2>Items</h2>
+        
         <hr />
         <div>
           <div className={styles.s}>
 
-          <div className={styles.y}>
-            <div>
+            <div className={styles.y}>
+              <div>
+
+              </div>
+              <div>
+                <p>Selling Price</p>
+              </div>
+
+
 
             </div>
-            <div>
-         <p>Selling Price</p>
+
+            <div className={styles.selector}>
+              <div >
+                <TextField onChange={handelSearch} sx={{ m: 1, width: 700 }} fullWidth label="Search item" id="fullWidth" variant="filled" />
+              </div>
+
+              <div className={styles.sel}>
+
+
+                <TextField onChange={handelAmount} sx={{ m: 1, width: 700 }} fullWidth label="0" id="fullWidth" variant="filled" />
+              </div>
+
             </div>
 
-            
-
-          </div>
-
-          <div className={styles.selector}>
-            <div >
-              <FormControl sx={{ m: 1, width: 500 }}>
-                <InputLabel id="demo-multiple-chip-label">Search item</InputLabel>
-                <Select
-                  labelId="demo-multiple-chip-label"
-                  id="demo-multiple-chip"
-                  multiple
-
-                  input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-                >
-
-                </Select>
-              </FormControl>
-            </div>
-
-            <div className={styles.sel}>
-
-              
-              <TextField sx={{ m: 1, width: 700 }} fullWidth label="0" id="fullWidth" variant="filled" />
-            </div>
-
-          </div>
           </div>
 
         </div>
+
         <div className={styles.addbtn}>
-        <Button variant="contained" size="small" >Add</Button>
+          <Button onClick={handelAddItem} variant="contained" size="small" >Add</Button>
 
         </div>
-        
+
 
       </section>
 
       <section>
+
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
             <TableHead>
@@ -108,21 +193,49 @@ export default function Home() {
                 <TableCell align="right">Action</TableCell>
 
               </TableRow>
+
             </TableHead>
 
           </Table>
         </TableContainer>
+
+        {
+          items.map(item => <div key={item.key}>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>{item.search.search}</TableCell>
+                    <TableCell align="right">{item.search.search}</TableCell>
+                    <TableCell align="right"><button onClick={add}>+</button>{item?.quantity.qun}<button onClick={sub}>-</button></TableCell>
+                    <TableCell align="right">{item.amount.amount}</TableCell>
+                    <TableCell align="right">{(item.amount.amount) * qun}</TableCell>
+                    <TableCell align="right"><DeleteIcon></DeleteIcon></TableCell>
+
+                  </TableRow>
+
+                </TableHead>
+
+              </Table>
+            </TableContainer>
+
+
+          </div>)
+        }
+
+
+
         <h2>Gross</h2>
         <div className={styles.grossinput}>
           <p>Discount Amt.</p>
-          <TextField fullWidth label="0" id="fullWidth" variant="filled" />
+          <TextField onChange={handelDiscount} fullWidth label="0" id="fullWidth" variant="filled" />
         </div>
         <div className={styles.grossinput}>
           <p>VAT/TAX</p>
           <div>
             <div className={styles.parsent}>
               <p>%</p>
-              <TextField fullWidth label="Phone Number" id="fullWidth" variant="filled" />
+              <TextField onBlur={handelVat} fullWidth label="vat" id="fullWidth" variant="filled" />
             </div>
 
           </div>
@@ -131,7 +244,7 @@ export default function Home() {
 
         <div className={styles.grossinput}>
           <h3>Total Billed </h3>
-          <h3>0 </h3>
+          <h3>{discounts ? discounts : totalBill} </h3>
         </div>
 
         <div className={styles.grossinput}>
@@ -161,27 +274,27 @@ export default function Home() {
 
       </section>
 
-        <section>
+      <section>
         <div className={styles.selector}>
-            <div >
-              <FormControl sx={{ m: 1, width: 500 }}>
-                <InputLabel id="demo-multiple-chip-label">Sell Via</InputLabel>
-                <Select
-                  labelId="demo-multiple-chip-label"
-                  id="demo-multiple-chip"
-                  multiple
+          <div >
+            <FormControl sx={{ m: 1, width: 500 }}>
+              <InputLabel id="demo-multiple-chip-label">Sell Via</InputLabel>
+              <Select
+                labelId="demo-multiple-chip-label"
+                id="demo-multiple-chip"
+                multiple
 
-                  input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-                >
+                input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+              >
 
-                </Select>
-              </FormControl>
-            </div>
-
-            
-
+              </Select>
+            </FormControl>
           </div>
-        </section>
+
+
+
+        </div>
+      </section>
 
       <section >
         <FormGroup >
--- pages/page-accounts.js
+++ pages/page-accounts.js
@@ -1,12 +1,15 @@
 import Head from "next/head";
+import { useState } from "react";
 import AccountDashBoard from "../components/AccountDashBoard";
 import Layout from "../components/Layout";
 // import styles from "../styles/selectorganization.module.css";
 
 export default function home() {
+
   return (
     <Layout>
       <Head>
+
         <title>Accounts Dashboard</title>
       </Head>
       <main className={{}}>
